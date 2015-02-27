4f37e98de0486ff30e1c40f5d531fec6ebd06c66
Daisuke Kobayashi <d5884jp@gmail.com>
2015-02-27 11:45:17 +0900

    Use process-send-string 4096 byte workaround over version 24.5.

--- init.el
+++ init.el
@@ -342,7 +342,7 @@ ORIGINAL が non-nil であれば最後に連結される."
                                         (ad-get-args ,p)))))))
 
       (when (and (= emacs-major-version 24)
-                 (= emacs-minor-version 4)) ;; 24.5 以降はどうなるかわからんので
+                 (>= emacs-minor-version 4))
         (defconst w32-pipe-limit 4096
           "Windows でのパイプバッファサイズ.")
 
